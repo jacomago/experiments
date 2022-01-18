965a5736e944802d51f55106057133f327c59289
Sky Brewer <jacobrewer@gmail.com>
2022-01-18 14:28:00 +0100

    draw a grid

--- schotter1/src/main.rs
+++ schotter1/src/main.rs
@@ -1,12 +1,45 @@
-use nannou::prelude::*;
+use nannou::{
+    color::{PLUM, STEELBLUE},
+    App, Frame, LoopMode,
+};
+
+const ROWS: u32 = 22;
+const COLS: u32 = 12;
+const SIZE: u32 = 30;
+const MARGIN: u32 = 35;
+const WIDTH: u32 = COLS * SIZE + 2 * MARGIN;
+const HEIGHT: u32 = ROWS * SIZE + 2 * MARGIN;
+const LINE_WIDTH: f32 = 0.06;
 
 fn main() {
-    nannou::sketch(view).run()
+    nannou::sketch(view)
+        .size(WIDTH, HEIGHT)
+        .loop_mode(LoopMode::loop_once())
+        .run()
 }
 
 fn view(app: &App, frame: Frame) {
     let draw = app.draw();
+    let gdraw = draw
+        .scale(SIZE as f32)
+        .scale_y(-1.0)
+        .x_y(COLS as f32 / -2.0 + 0.5, ROWS as f32 / -2.0 + 0.5);
+
     draw.background().color(PLUM);
-    draw.ellipse().color(STEELBLUE);
+
+    for y in 0..ROWS {
+        for x in 0..COLS {
+            let cdraw = gdraw.x_y(x as f32, y as f32);
+            cdraw
+                .rect()
+                .no_fill()
+                .stroke(STEELBLUE)
+                .stroke_weight(LINE_WIDTH)
+                .w_h(1.0, 1.0)
+                .x_y(0.0, 0.0)
+                .rotate(0.0);
+        }
+    }
+
     draw.to_frame(app, &frame).unwrap();
 }
