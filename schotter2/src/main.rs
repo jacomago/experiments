bd5af361766b7da8ddffbc91bd7b7fc0dca11792
Sky Brewer <jacobrewer@gmail.com>
2022-01-18 14:46:52 +0100

    no change on mouse move or keyboard input

--- schotter2/src/main.rs
+++ schotter2/src/main.rs
@@ -2,7 +2,7 @@ use nannou::{
     color::{PLUM, STEELBLUE},
     event::Update,
     prelude::PI,
-    rand::random_range,
+    rand::{prelude::StdRng, random_range, Rng, SeedableRng},
     App, Frame, LoopMode,
 };
 
@@ -17,11 +17,13 @@ const LINE_WIDTH: f32 = 0.06;
 fn main() {
     nannou::app(model)
         .update(update)
-        .loop_mode(LoopMode::loop_once())
+        .loop_mode(LoopMode::wait())
         .run();
 }
 
-struct Model {}
+struct Model {
+    random_seed: u64,
+}
 
 fn model(app: &App) -> Model {
     let _window = app
@@ -31,12 +33,13 @@ fn model(app: &App) -> Model {
         .view(view)
         .build()
         .unwrap();
-    Model {}
+    let random_seed = random_range(0, 1000000);
+    Model { random_seed }
 }
 
 fn update(_app: &App, _model: &mut Model, _update: Update) {}
 
-fn view(app: &App, _model: &Model, frame: Frame) {
+fn view(app: &App, model: &Model, frame: Frame) {
     let draw = app.draw();
     let gdraw = draw
         .scale(SIZE as f32)
@@ -45,13 +48,15 @@ fn view(app: &App, _model: &Model, frame: Frame) {
 
     draw.background().color(PLUM);
 
+    let mut rng = StdRng::seed_from_u64(model.random_seed);
+
     for y in 0..ROWS {
         for x in 0..COLS {
             let cdraw = gdraw.x_y(x as f32, y as f32);
             let factor = y as f32 / ROWS as f32;
-            let x_offset = factor * random_range(-0.5, 0.5);
-            let y_offset = factor * random_range(-0.5, 0.5);
-            let rotation = factor * random_range(-PI / 4.0, PI / 4.0);
+            let x_offset = factor * rng.gen_range(-0.5..0.5);
+            let y_offset = factor * rng.gen_range(-0.5..0.5);
+            let rotation = factor * rng.gen_range(-PI / 4.0..PI / 4.0);
             cdraw
                 .rect()
                 .no_fill()
