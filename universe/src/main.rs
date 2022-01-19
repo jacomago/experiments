aa4aaef23d0171ded57dc4fcaef3623bb078131c
Sky Brewer <jacobrewer@gmail.com>
2022-01-19 16:14:01 +0100

    add keyboard control and example image

--- universe/src/main.rs
+++ universe/src/main.rs
@@ -7,7 +7,10 @@ use nannou::{
 // @Mactuitui
 
 fn main() {
-    nannou::app(model).update(update).run();
+    nannou::app(model)
+        .update(update)
+        .loop_mode(LoopMode::wait())
+        .run();
 }
 
 struct Thing {
@@ -21,20 +24,49 @@ impl Thing {
     }
 }
 
+struct Parameters {
+    speed: f64,
+    sn_ratio: f64,
+    loops: i32,
+}
+
 struct Model {
     things: Vec<Thing>,
     noise: Perlin,
-    _window: window::Id,
+    parameters: Parameters,
 }
 
-const N_THINGS: usize = 500;
-const SIZE: usize = 300;
+const N_THINGS: usize = 1000;
+const SIZE: usize = 500;
+
+fn key_pressed(app: &App, model: &mut Model, key: Key) {
+    match key {
+        Key::S => app
+            .main_window()
+            .capture_frame("images/".to_owned() + &app.exe_name().unwrap() + ".png"),
+        Key::Up => model.parameters.sn_ratio += 0.001,
+        Key::Down => {
+            if model.parameters.sn_ratio > 0.0 {
+                model.parameters.sn_ratio -= 0.001;
+            }
+        }
+        Key::Right => model.parameters.speed += 1.0,
+        Key::Left => {
+            if model.parameters.speed > 0.0 {
+                model.parameters.speed -= 0.1;
+            }
+        }
+        _other_key => {}
+    }
+}
 
 fn model(app: &App) -> Model {
     let _window = app
         .new_window()
+        .title(app.exe_name().unwrap())
         .size(SIZE as u32, SIZE as u32)
         .view(view)
+        .key_pressed(key_pressed)
         .build()
         .unwrap();
 
@@ -50,13 +82,17 @@ fn model(app: &App) -> Model {
     Model {
         things,
         noise,
-        _window,
+        parameters: Parameters {
+            speed: 120.0,
+            sn_ratio: 0.005,
+            loops: 50,
+        },
     }
 }
 
 fn update(app: &App, model: &mut Model, _update: Update) {
-    let time = app.elapsed_frames() as f64 / 120.0;
-    let sn = 0.01 * time.cos() as f64 * 0.005;
+    let time = app.elapsed_frames() as f64 / model.parameters.speed;
+    let sn =  time.cos() as f64 * model.parameters.sn_ratio;
     for thing in model.things.iter_mut() {
         thing.positions.clear();
         thing.positions.push(vec2(
@@ -64,7 +100,7 @@ fn update(app: &App, model: &mut Model, _update: Update) {
             (random::<f32>() - 0.5) * SIZE as f32,
         ));
 
-        for _ in 0..50 {
+        for _ in 0..model.parameters.loops {
             let last_position = thing.positions[0];
             thing.positions.insert(
                 0,
