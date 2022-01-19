0798f14960792e7f1b9cdafceccc5c37742e92ef
Sky Brewer <jacobrewer@gmail.com>
2022-01-19 17:03:17 +0100

    create a quick template

---
+++ template/src/main.rs
@@ -0,0 +1,65 @@
+use nannou::prelude::*;
+
+fn main() {
+    nannou::app(model)
+        .update(update)
+        .loop_mode(LoopMode::wait())
+        .run();
+}
+
+struct Fields {
+    field: f64,
+}
+
+struct Model {
+    fields: Fields,
+}
+
+const SIZE: usize = 500;
+
+fn key_pressed(app: &App, model: &mut Model, key: Key) {
+    match key {
+        Key::S => app
+            .main_window()
+            .capture_frame("images/".to_owned() + &app.exe_name().unwrap() + ".png"),
+        Key::Up => model.fields.field += 0.001,
+        Key::Down => {
+            if model.fields.field > 0.0 {
+                model.fields.field -= 0.001;
+            }
+        }
+        Key::Right => model.fields.field += 1.0,
+        Key::Left => {
+            if model.fields.field > 0.0 {
+                model.fields.field -= 0.1;
+            }
+        }
+        _other_key => {}
+    }
+}
+
+fn model(app: &App) -> Model {
+    let _window = app
+        .new_window()
+        .title(app.exe_name().unwrap())
+        .size(SIZE as u32, SIZE as u32)
+        .view(view)
+        .key_pressed(key_pressed)
+        .build()
+        .unwrap();
+
+    Model {
+        fields: Fields { field: 120.0 },
+    }
+}
+
+fn update(_app: &App, _model: &mut Model, _update: Update) {}
+
+fn view(app: &App, _model: &Model, frame: Frame) {
+    let draw = app.draw();
+
+    draw.background().color(PLUM);
+    draw.ellipse().color(STEELBLUE);
+
+    draw.to_frame(app, &frame).unwrap();
+}
