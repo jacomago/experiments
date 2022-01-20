cb36fe1fdfa5a663e8f6a987c11414ba8bb6ab93
Sky Brewer <jacobrewer@gmail.com>
2022-01-20 17:58:27 +0100

    vector line to mouse

---
+++ nature_of_code/vectors/src/bin/vectors.rs
@@ -0,0 +1,75 @@
+use nannou::prelude::*;
+
+fn main() {
+    nannou::app(model)
+        .update(update)
+        .loop_mode(LoopMode::wait())
+        .run();
+}
+
+struct Fields {
+    field: f32,
+}
+
+struct Model {
+    fields: Fields,
+}
+
+const SIZE: usize = 500;
+
+fn key_pressed(app: &App, model: &mut Model, key: Key) {
+    match key {
+        Key::S => app.main_window().capture_frame(format!(
+            "{}/images/{app_name}.png",
+            &app.exe_name().unwrap(),
+            app_name = &app.exe_name().unwrap()
+        )),
+        Key::Up => model.fields.field += 0.001,
+        Key::Down => {
+            if model.fields.field > 0.0 {
+                model.fields.field -= 0.001;
+            }
+        }
+        Key::Right => model.fields.field += 1.0,
+        Key::Left => {
+            if model.fields.field > 0.0 {
+                model.fields.field -= 0.1;
+            }
+        }
+        _other_key => {}
+    }
+}
+
+fn model(app: &App) -> Model {
+    let _window = app
+        .new_window()
+        .title(app.exe_name().unwrap())
+        .size(SIZE as u32, SIZE as u32)
+        .view(view)
+        .key_pressed(key_pressed)
+        .build()
+        .unwrap();
+
+    Model {
+        fields: Fields { field: 120.0 },
+    }
+}
+
+fn update(_app: &App, _model: &mut Model, _update: Update) {}
+
+fn view(app: &App, model: &Model, frame: Frame) {
+    let draw = app.draw();
+
+    draw.background().color(PLUM);
+    let mouse_pos = app.mouse.position();
+    draw.rect()
+        .xy(app.window_rect().top_left())
+        .h(10.0)
+        .w(mouse_pos.length())
+        .color(BLACK);
+    draw.line()
+        .end(app.mouse.position() * model.fields.field)
+        .color(STEELBLUE);
+
+    draw.to_frame(app, &frame).unwrap();
+}
