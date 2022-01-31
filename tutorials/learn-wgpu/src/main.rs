8ae3b5ba4d7838ffb4e8fdeea80804d06a7056dc
Sky Brewer <jacobrewer@gmail.com>
2022-01-31 18:05:55 +0100

    change shader on space

--- tutorials/learn-wgpu/src/main.rs
+++ tutorials/learn-wgpu/src/main.rs
@@ -12,7 +12,9 @@ struct State {
     config: wgpu::SurfaceConfiguration,
     size: winit::dpi::PhysicalSize<u32>,
     color: wgpu::Color,
+    use_color: bool,
     render_pipeline: wgpu::RenderPipeline,
+    color_pipeline: wgpu::RenderPipeline,
 }
 
 impl State {
@@ -97,6 +99,44 @@ impl State {
             },
             multiview: None, // 5.
         });
+
+        let shader = device.create_shader_module(&include_wgsl!("color.wgsl"));
+        let color_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+            label: Some("Color Pipeline"),
+            layout: Some(&render_pipeline_layout),
+            vertex: wgpu::VertexState {
+                module: &shader,
+                entry_point: "vs_main", // 1.
+                buffers: &[],           // 2.
+            },
+            fragment: Some(wgpu::FragmentState {
+                // 3.
+                module: &shader,
+                entry_point: "fs_main",
+                targets: &[wgpu::ColorTargetState {
+                    // 4.
+                    format: config.format,
+                    blend: Some(wgpu::BlendState::REPLACE),
+                    write_mask: wgpu::ColorWrites::ALL,
+                }],
+            }),
+            primitive: wgpu::PrimitiveState {
+                topology: wgpu::PrimitiveTopology::TriangleList, // 1.
+                strip_index_format: None,
+                front_face: wgpu::FrontFace::Ccw, // 2.
+                cull_mode: Some(wgpu::Face::Back),
+                // Setting this to anything other than Fill requires Features::NON_FILL_POLYGON_MODE
+                polygon_mode: wgpu::PolygonMode::Fill,
+                ..Default::default()
+            },
+            depth_stencil: None, // 1.
+            multisample: wgpu::MultisampleState {
+                count: 1,                         // 2.
+                mask: !0,                         // 3.
+                alpha_to_coverage_enabled: false, // 4.
+            },
+            multiview: None, // 5.
+        });
         let color = wgpu::Color {
             r: 0.1,
             g: 0.2,
@@ -110,7 +150,9 @@ impl State {
             config,
             size,
             color,
+            use_color: false,
             render_pipeline,
+            color_pipeline,
         }
     }
 
@@ -143,6 +185,19 @@ impl State {
                 };
                 true
             }
+            WindowEvent::KeyboardInput {
+                input:
+                    KeyboardInput {
+                        state,
+                        virtual_keycode: Some(VirtualKeyCode::Space),
+                        ..
+                    },
+                ..
+            } => {
+                self.use_color = *state == ElementState::Released;
+                true
+            }
+
             _ => false,
         }
     }
@@ -174,7 +229,11 @@ impl State {
             });
 
             // NEW!
-            render_pass.set_pipeline(&self.render_pipeline); // 2.
+            render_pass.set_pipeline(if self.use_color {
+                &self.render_pipeline
+            } else {
+                &self.color_pipeline
+            }); // 2.
             render_pass.draw(0..3, 0..1); // 3.
         }
 
