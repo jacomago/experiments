8be121e7d002f5a348120ed5395303124f650a07
Sky Brewer <jacobrewer@gmail.com>
2022-02-11 15:55:50 +0100

    linear interp

--- log_density/src/main.rs
+++ log_density/src/main.rs
@@ -1,3 +1,4 @@
+use log_density::renderer::LinearRenderer;
 use nannou::noise::{NoiseFn, Perlin};
 use nannou::prelude::*;
 use nannou::rand::prelude::ThreadRng;
@@ -5,11 +6,11 @@ use nannou::rand::{thread_rng, Rng};
 use rand_distr::StandardNormal;
 
 fn main() {
-    nannou::app(model).update(update).run();
+    nannou::app(model).run();
 }
 
 struct Blob {
-    points: Vec<Vec2>,
+    renderer: LinearRenderer,
     rng: ThreadRng,
     noise: Perlin,
 }
@@ -27,50 +28,59 @@ fn gen_point(
         (noise.get([xy.x as f64, xy.y as f64]) - 0.5) as f32,
         (noise.get([(xy.y - 1.1) as f64, (xy.x + 1.1) as f64, perlin_factor]) - 0.5) as f32,
     );
-    scale * xy + nxy
+    vec2(400.0, 400.0) + scale * xy + nxy
 }
 
-impl Blob {
-    fn new(_int: usize, rng: ThreadRng, noise: Perlin) -> Self {
-        let points = Vec::new();
-        Blob { points, rng, noise }
-    }
-    fn new_point(&mut self, noise_scale: f32, perlin_factor: f64, scale: f32) {
-        self.points.push(gen_point(
-            noise_scale,
-            &mut self.rng,
-            self.noise,
-            perlin_factor,
-            scale,
-        ));
-    }
-    fn new_points(&mut self, noise_scale: f32, perlin_factor: f64, scale: f32, amount: usize) {
-        (0..amount).for_each(|f| self.new_point(noise_scale, perlin_factor, scale));
-    }
-    fn last_point(&self) -> Vec2 {
-        *self.points.last().unwrap_or(&vec2(0.0, 0.0))
-    }
-    fn last_points(&self, amount: usize) -> Vec<Vec2> {
-        (*self
-            .points
-            .chunks_exact(amount)
-            .last()
-            .unwrap_or(&[Vec2::ZERO]))
-        .to_vec()
-    }
-}
 struct Model {
     blob: Blob,
+    texture: wgpu::Texture,
     rate: usize,
     scale: f32,
     perlin_factor: f64,
-    noise_scale: f32
+    noise_scale: f32,
+}
+
+impl Blob {
+    fn new(w: usize, h: usize, rng: ThreadRng, noise: Perlin) -> Self {
+        let renderer = LinearRenderer::new(w, h);
+        Blob {
+            renderer,
+            rng,
+            noise,
+        }
+    }
+
+    fn gen(&mut self, rate: usize, noise_scale: f32, perlin_factor: f64, scale: f32) {
+        (0..rate).for_each(|_i| {
+            self.renderer.add(gen_point(
+                noise_scale,
+                &mut self.rng,
+                self.noise,
+                perlin_factor,
+                scale,
+            ))
+        });
+    }
 }
 
 fn key_pressed(app: &App, model: &mut Model, key: Key) {
     interaction::key_pressed(app, &mut model.scale, &mut model.noise_scale, key);
+
+    match key {
+        Key::G => {
+            model.blob.gen(
+                model.rate,
+                model.noise_scale,
+                model.perlin_factor,
+                model.scale,
+            );
+        }
+        _other_key => {}
+    }
 }
-const SIZE: usize = 500;
+
+const SIZE: usize = 1000;
+
 fn model(app: &App) -> Model {
     let _window = app
         .new_window()
@@ -83,27 +93,48 @@ fn model(app: &App) -> Model {
 
     let rng = thread_rng();
     let noise = Perlin::new();
-    let blob = Blob::new(0,  rng, noise);
-    let rate = 100;
-    Model { blob, rate , scale: 100.0, perlin_factor: 0.4, noise_scale: 50.0}
-}
 
-fn update(_app: &App, model: &mut Model, _update: Update) {
-    model.blob.new_points(model.noise_scale, model.perlin_factor, model.scale, model.rate);
+    let rate = 1000000;
+    let scale = 100.0;
+    let perlin_factor = 0.4;
+    let noise_scale = 50.0;
+
+    let wrect = app.window_rect();
+
+    let window = app.main_window();
+    let texture = wgpu::TextureBuilder::new()
+        .size([wrect.w() as u32, wrect.h() as u32])
+        .format(wgpu::TextureFormat::Rgba8Unorm)
+        .usage(wgpu::TextureUsages::COPY_DST | wgpu::TextureUsages::TEXTURE_BINDING)
+        .build(window.device());
+
+    let mut blob = Blob::new(wrect.w() as usize, wrect.h() as usize, rng, noise);
+    blob.gen(rate, noise_scale, perlin_factor, scale);
+    blob.renderer.render(srgba(0.1, 0.0,0.0,1.0), srgba(1.0,1.0,1.0,1.0), 1.0 / 2.2);
+
+    Model {
+        blob,
+        texture,
+        rate,
+        scale,
+        perlin_factor,
+        noise_scale,
+    }
 }
 
 fn view(app: &App, model: &Model, frame: Frame) {
-    // Prepare to draw.
+    frame.clear(BLACK);
+
+    let flat_samples = model.blob.renderer.img().as_flat_samples();
+    model.texture.upload_data(
+        app.main_window().device(),
+        &mut *frame.command_encoder(),
+        flat_samples.as_slice(),
+    );
+
     let draw = app.draw();
 
-    if app.elapsed_frames() == 0 {
-        draw.background().color(BLACK);
-    }
-    for point in model.blob.last_points(model.rate) {
-        draw.ellipse()
-            .xy(point)
-            .radius(1.0)
-            .rgba(1.0, 1.0, 1.0, 0.9);
-    }
+    draw.texture(&model.texture);
+
     draw.to_frame(app, &frame).unwrap();
 }
---
+++ log_density/src/renderer.rs
@@ -0,0 +1,60 @@
+use nannou::{
+    color::Gradient,
+    image::{self, RgbaImage},
+    prelude::*,
+};
+
+pub struct LinearRenderer {
+    hits: Vec<Vec<usize>>,
+    w: usize,
+    h: usize,
+    img: RgbaImage,
+}
+
+impl LinearRenderer {
+    pub fn new(w: usize, h: usize) -> Self {
+        let hits = vec![vec![0; w]; h];
+        let img = RgbaImage::new(w as u32, h as u32);
+        LinearRenderer { hits, w, h, img }
+    }
+
+    pub fn add(&mut self, xy: Vec2) {
+        let xint = xy.x.floor() as i32;
+        let yint = xy.y.floor() as i32;
+        if xint >= 0 && xint < self.w as i32 && yint >= 0 && yint < self.h as i32 {
+            self.hits[yint as usize][xint as usize] += 1;
+        }
+    }
+
+    pub fn render(&mut self, back: Srgba, fore: Srgba, gamma: f32) {
+        let mx = *self
+            .hits
+            .iter()
+            .map(|v| v.iter().max())
+            .max()
+            .unwrap()
+            .unwrap() as f32;
+        for (x, row) in self.hits.iter().enumerate() {
+            for (y, col) in row.iter().enumerate() {
+                let pixel = if *col > 0 {
+                    let alpha = (*col as f32 / mx).pow(gamma);
+                    let grad = Gradient::new(vec![back.into_linear(), fore.into_linear()]);
+                    let new_c = Srgba::from_linear(grad.get(alpha));
+                    image::Rgba([
+                        map_range(new_c.red, 0.0, 1.0, 0, std::u8::MAX),
+                        map_range(new_c.green, 0.0, 1.0, 0, std::u8::MAX),
+                        map_range(new_c.blue, 0.0, 1.0, 0, std::u8::MAX),
+                        map_range(new_c.alpha, 0.0, 1.0, 0, std::u8::MAX),
+                    ])
+                } else {
+                    image::Rgba([0, 0, 0, 0])
+                };
+
+                self.img.put_pixel(x as u32, y as u32, pixel);
+            }
+        }
+    }
+    pub fn img(&self) -> &RgbaImage {
+        &self.img
+    }
+}
