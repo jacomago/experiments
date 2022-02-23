63dd28e558592e557f9dcea7028a3c74a94fd471
Sky Brewer <jacobrewer@gmail.com>
2022-02-23 16:39:56 +0100

    twisty twisty

--- learning/learn-wgpu/src/main.rs
+++ learning/learn-wgpu/src/main.rs
@@ -116,6 +116,7 @@ impl InstanceRaw {
     }
 }
 
+const ROTATION_SPEED: f32 = 2.0 * std::f32::consts::PI / 60.0;
 struct Instance {
     position: cgmath::Vector3<f32>,
     rotation: cgmath::Quaternion<f32>,
@@ -402,7 +403,7 @@ impl State {
         let instance_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
             label: Some("Instance Buffer"),
             contents: bytemuck::cast_slice(&instance_data),
-            usage: wgpu::BufferUsages::VERTEX,
+            usage: wgpu::BufferUsages::VERTEX | wgpu::BufferUsages::COPY_DST,
         });
 
         Self {
@@ -475,6 +476,22 @@ impl State {
             0,
             bytemuck::cast_slice(&[self.camera_uniform]),
         );
+
+        for instance in &mut self.instances {
+            let amount = cgmath::Quaternion::from_angle_y(cgmath::Rad(ROTATION_SPEED));
+            let current = instance.rotation;
+            instance.rotation = quat_mul(amount, current);
+        }
+        let instance_data = self
+            .instances
+            .iter()
+            .map(Instance::to_raw)
+            .collect::<Vec<_>>();
+        self.queue.write_buffer(
+            &self.instance_buffer,
+            0,
+            bytemuck::cast_slice(&instance_data),
+        );
     }
 
     fn render(&mut self) -> Result<(), wgpu::SurfaceError> {
@@ -525,6 +542,33 @@ impl State {
     }
 }
 
+fn quat_mul(q: cgmath::Quaternion<f32>, r: cgmath::Quaternion<f32>) -> cgmath::Quaternion<f32> {
+    // This block uses quaternions of the form of
+
+    // q=q0+iq1+jq2+kq3
+
+    // and
+
+    // r=r0+ir1+jr2+kr3.
+
+    // The quaternion product has the form of
+
+    // t=q×r=t0+it1+jt2+kt3,
+
+    // where
+
+    // t0=(r0 q0 − r1 q1 − r2 q2 − r3 q3)
+    // t1=(r0 q1 + r1 q0 − r2 q3 + r3 q2)
+    // t2=(r0 q2 + r1 q3 + r2 q0 − r3 q1)
+    // t3=(r0 q3 − r1 q2 + r2 q1 + r3 q0
+
+    let w = r.s * q.s - r.v.x * q.v.x - r.v.y * q.v.y - r.v.z * q.v.z;
+    let xi = r.s * q.v.x + r.v.x * q.s - r.v.y * q.v.z + r.v.z * q.v.y;
+    let yj = r.s * q.v.y + r.v.x * q.v.z + r.v.y * q.s - r.v.z * q.v.x;
+    let zk = r.s * q.v.z - r.v.x * q.v.y + r.v.y * q.v.x + r.v.z * q.s;
+
+    cgmath::Quaternion::new(w, xi, yj, zk)
+}
 fn main() {
     env_logger::init();
     let event_loop = EventLoop::new();
