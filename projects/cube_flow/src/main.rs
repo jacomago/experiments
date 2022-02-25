5c42c3a082971d38d95f714f6ff7e2cb5b8a2f2d
Sky Brewer <jacobrewer@gmail.com>
2022-02-25 16:43:00 +0100

    shifted cubes

--- projects/cube_flow/src/main.rs
+++ projects/cube_flow/src/main.rs
@@ -1,54 +1,29 @@
 use nannou::prelude::*;
 
 fn main() {
-    nannou::app(model).update(update).run();
+    nannou::sketch(view).run()
 }
 
-struct Model {
-    field_up: f32,
-    field_left: f32,
-    size: f32,
-}
-
-const SIZE: usize = 500;
-
-fn key_pressed(app: &App, model: &mut Model, key: Key) {
-    interaction::key_pressed(app, &mut model.field_up, &mut model.field_left, key);
-}
-
-fn model(app: &App) -> Model {
-    let _window = app
-        .new_window()
-        .title(app.exe_name().unwrap())
-        .size(SIZE as u32, SIZE as u32)
-        .view(view)
-        .key_pressed(key_pressed)
-        .build()
-        .unwrap();
-
-    Model {
-        field_up: 120.0,
-        field_left: 1.0,
-        size: 10.0,
-    }
-}
-
-fn update(_app: &App, _model: &mut Model, _update: Update) {}
-
-fn view(app: &App, model: &Model, frame: Frame) {
+fn view(app: &App, frame: Frame) {
+    frame.clear(DIMGRAY);
     let draw = app.draw();
     let t = app.elapsed_frames() as f32 * 0.1;
     let rect = app.window_rect();
     draw.background().color(PLUM);
+
     let w = rect.w() * 0.09;
     let margin = rect.w() * 0.01;
-    for x in 0..10 {
-        draw.rect()
-            .x(rect.left() + (w + margin) * (x as f32 + 0.5))
-            .w(w)
-            .h(20.0 + 50.0 * (t + x as f32 * 0.1).sin())
-            .color(STEELBLUE);
-    }
-
+    let tris = (0..10)
+        .flat_map(|i| {
+            let centre = vec3(rect.left() + (w + margin) * (i as f32 + 0.5), 0.0, 0.0);
+            let size = vec3(w, 20.0 + 50.0 * (t + i as f32 * 0.1).sin(), 20.0);
+            geom::Cuboid::from_xyz_whd(centre, size)
+                .triangles_iter().collect::<Vec<_>>()
+        })
+        .map(|tri| tri.map_vertices(|v| (v, STEELBLUE)));
+    draw.mesh()
+        .tris_colored(tris)
+        .z_radians(0.33)
+        .x_radians(0.33);
     draw.to_frame(app, &frame).unwrap();
 }
